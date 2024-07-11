283a0a0756e0dd95d575359f0ecffa68cead2ac1
LilDiabetX <benjamin.t.decker@gmail.com>
2024-07-11 11:10:04 +0200

    slant stack cython

--- setup.py
+++ setup.py
@@ -1,26 +1,24 @@
-import subprocess
-import os
 from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Build import cythonize
 
-
-
-# if not os.path.exists("./lib/"):
-#    os.makedirs("./lib/")
-
-
+fftw_include_dir = '/usr/local/include'
+lib_dir = '/usr/local/lib'
 
 extensions = [
-    Extension("slant_stack", 
-                ["slant_stack.pyx"],
+    Extension(name="slant_stack", 
+                sources=["slant_stack.pyx"],
                 language="c++",
+                include_dirs=[fftw_include_dir],
+                library_dirs=[lib_dir],
+                libraries=["fftw3", "omp"],
                 extra_compile_args=["-O3"],
                 extra_link_args=["-O3"]),
 ]
 
 setup(
-    ext_modules = cythonize(extensions)
+    name="slant_stack",
+    ext_modules=cythonize(extensions),
 )
 
 
